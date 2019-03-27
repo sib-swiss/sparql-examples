01aa5649e5e6bf4707810196f8f8155986536590
Jerven Bolleman <jerven.bolleman@sib.swiss>
2019-03-27 14:35:40 +0100

    If riot is not installed on a machine use rapper to generate the one turtle file

--- convertToOneTurtle.sh
+++ convertToOneTurtle.sh
@@ -3,5 +3,9 @@ rm examples.ttl
 echo "# baseURI: http://sparql.uniprot.org/.well-known/sparql-examples#" > examples.ttl
 echo "# imports: http://purl.uniprot.org/core/ " >> examples.ttl
 echo "# prefix: ex" >> examples.ttl
-riot --formatted=turtle *.ttl  >> examples.ttl
-
+if [ ! -z $(which riot) ]
+then
+  riot --formatted=turtle *.ttl  >> examples.ttl
+else
+  rapper -q -i turtle <(cat [1-9]*.ttl prefixes.ttl) -o turtle  >> examples.ttl
+fi
