41d6f1dcfcdff35f84829d239bf74cbc8d3ab6f5
Jerven bolleman <jerven.bolleman@sib.swiss>
2019-05-20 21:16:52 +0200

    Add all rhea examples to the example query set so that it can be reused for sparql.rhea-db.org

--- check.sh
+++ check.sh
@@ -4,11 +4,17 @@ set -o errexit # -e does not work in Shebang-line!
 set -o pipefail
 set -o nounset
 
-for i in $(ls [1-9]*.ttl);
-do 
-    if [ $(grep -c "ex:${i:0:${#i}-4}" $i) -lt 1 ];
+for i in $(ls */[1-9]*.ttl);
+do
+    f=$(echo $i | cut -f 2 -d '/' )	
+    if [ $(grep -c "ex:${f:0:${#f}-4}" $i) -lt 1 ];
     then 
         echo $i;
         exit 1;
     fi;
+    if [ $(rapper -q -i turtle -c $i) ];
+    then
+	  echo $i;
+	  exit 2;
+    fi 
 done
--- convertToOneTurtle.sh
+++ convertToOneTurtle.sh
@@ -1,11 +1,28 @@
 #!/usr/bin/bash
 rm examples.ttl
+
+
 echo "# baseURI: http://sparql.uniprot.org/.well-known/sparql-examples#" > examples.ttl
 echo "# imports: http://purl.uniprot.org/core/ " >> examples.ttl
 echo "# prefix: ex" >> examples.ttl
+
+project="*"
+while getopts uhrs option; do
+    case "$option" in
+        u) project="uniprot";;
+        h) project="hamap";;
+        r) project="rhea";;
+        s) project="swisslipids";;
+        h) help; exit 0;;
+        *) help; exit 1;;
+    esac
+done
+
+
 if which riot
 then
-  riot --formatted=turtle *.ttl  >> examples.ttl
+  riot --formatted=turtle prefixes.ttl  > examples.ttl
+  riot --formatted=turtle $project/*.ttl  >> examples.ttl
 else
-  rapper -q -i turtle <(cat [1-9]*.ttl prefixes.ttl) -o turtle  >> examples.ttl
+  rapper -q -i turtle <(cat ${project}/[1-9]*.ttl prefixes.ttl) -o turtle  > examples.ttl
 fi
