42d1af050356182ed6bb355659b7e3f42276ea80
Jerven bolleman <jerven.bolleman@sib.swiss>
2020-08-25 11:08:36 +0200

    Make sure queries are valid sparql by validating with jena sparql engine. Also does a few more swisslipids examples.

--- check.sh
+++ check.sh
@@ -4,6 +4,8 @@ set -o errexit # -e does not work in Shebang-line!
 set -o pipefail
 set -o nounset
 
+prefixes=$(sparql --results=TSV --data=prefixes.ttl "PREFIX sh:<http://www.w3.org/ns/shacl#> SELECT ?s WHERE {?pn sh:prefix ?prefix ; sh:namespace ?namespaceI . BIND(CONCAT('PREFIX ',?prefix, ':<',(STR(?namespaceI)),'>') AS ?s)}"|grep -v "^\?s$" |tr -d '"')
+
 for i in $(ls */[1-9]*.ttl);
 do
     f=$(echo $i | cut -f 2 -d '/' )	
@@ -17,4 +19,16 @@ do
 	  echo $i;
 	  exit 2;
     fi 
+    q=$(sparql --results=TSV --data=$i "PREFIX sh:<http://www.w3.org/ns/shacl#> SELECT ?qs WHERE {?q sh:select|sh:describe|sh:construct|sh:ask ?qs}"|grep -vP "^\?qs$");
+    pq="${q:1:${#q}-2}";
+    if [[ ! -z "$pq" ]]
+    then
+        query="$prefixes $pq"
+        if [[ ! $(echo -e $query | sed 's|\\"|"|g' | sparql --query=/dev/stdin) ]]
+        then
+            echo $i
+            echo -e "__${pq}__"
+            exit 3;
+        fi
+    fi
 done
