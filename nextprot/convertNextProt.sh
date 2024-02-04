d504c3b7b969de047db4607093ea80dcc912a44a
Jerven Bolleman <jerven.bolleman@sib.swiss>
2024-02-04 15:02:21 +0100

    Improve script to convert all the nextprot examples

--- nextprot/convertNextProt.sh
+++ nextprot/convertNextProt.sh
@@ -3,10 +3,11 @@ while read -r i;
 do
 	id=$(echo $i | jq '.[0]')
 	title=$(echo $i | jq '.[2]')
-	sparql=$(echo $i | jq '.[1]'|sed -e 's| :| np:|g'|sed -e 's|/:|/np:|g'|sed -e 's|;:|; np:|g' |sed -e 's|cv:|nextprot_cv:|g')
-	filename="${id:1:-1}.ttl"
+	sparql=$(echo $i | jq '.[1]'| sed -e 's|entry:|nextprot:|g' |sed -e 's|cv:|nextprot_cv:|g')
+	sparql=$(echo $sparql| sed -e 's|chebi\:CHEBI\_|CHEBI:|g')
+	filename="${id:1:-1}.ttl":
 
-	echo "prefix ex: <https://sparql.nextprot.org/.well-known/sparql-examples>" >$filename
+	echo "prefix ex: <https://sparql.nextprot.org/.well-known/sparql-examples>" > $filename
 	echo "prefix sh: <http://www.w3.org/ns/shacl#> "  >> $filename
 	echo "prefix rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>"  >> $filename
 	echo "prefix rdfs:<http://www.w3.org/2000/01/rdf-schema#>"  >> $filename
@@ -14,5 +15,9 @@ do
 	echo " sh:prefixes _:sparql_examples_prefixes ;" >> $filename
 	echo -e "  rdfs:comment '''${title:1:-1}''' ;"  >> $filename
 	echo -e "  sh:select '''${sparql:1:-1}'''"  >> $filename
+	while read t;
+	do 
+		echo " schema:keyword $t;" >> $filename
+	done < <(echo $i| jq '.[3] | .'| grep -v "\[" | grep -v "\]"|tr ',' ' '|grep -v 'snorql-only')
 	echo "."  >> $filename
-done < <(curl 'https://api.nextprot.org/queries/tutorial.json?snorql=true' | jq -c '. [] | [ .publicId, .sparql, .title ]')
+done < <(curl 'https://api.nextprot.org/queries/tutorial.json?snorql=true' | jq -c '. [] | [ .publicId, .sparql, .title, [.tags] ]')
