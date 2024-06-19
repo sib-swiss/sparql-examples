72fd709412f76c661e1541967d4c047a4c195d5a
Jerven Bolleman <jerven.bolleman@sib.swiss>
2024-06-19 14:36:04 +0200

    Add support in the mermaid diagram for optional and service clauses.

--- src/test/java/swiss/sib/rdf/sparql/examples/SparqlInRdfToMermaidTest.java
+++ src/test/java/swiss/sib/rdf/sparql/examples/SparqlInRdfToMermaidTest.java
@@ -58,6 +58,45 @@ public class SparqlInRdfToMermaidTest {
 			  ?rhea rh:isTransport ?isTransport .
 			  ?rhea rh:equation ?equation .
 			}''' . """;
+	String rhea9Anon = """
+			prefix ex: <https://sparql.rhea-db.org/.well-known/sparql-examples/>
+			prefix sh: <http://www.w3.org/ns/shacl#>
+			prefix schema:<https://schema.org/>
+			prefix rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
+			prefix rdfs:<http://www.w3.org/2000/01/rdf-schema#>
+			
+			_:sparql_examples_prefixes sh:declare _:prefix_rhea .
+			_:prefix_rhea
+			  sh:prefix "rh" ;
+			  sh:namespace "http://rdf.rhea-db.org/"^^xsd:anyURI .
+			
+			_:sparql_examples_prefixes sh:declare _:prefix_ec .
+			_:prefix_ec 
+			  sh:prefix "ec" ;
+			  sh:namespace "http://purl.uniprot.org/enzyme/"^^xsd:anyURI .
+
+
+			ex:9
+			    a sh:SPARQLSelectExecutable, sh:SPARQLExecutable ;
+			    sh:prefixes _:sparql_examples_prefixes ;
+			    schema:target <https://sparql.rhea-db.org/sparql/> ;
+			    rdfs:comment "Select all Rhea reactions mapped to enzyme classification (EC numbers)"^^rdf:html ;
+			    sh:select '''# Query 9
+			# Select all Rhea reactions mapped to EC numbers (enzyme classification)
+			#
+			# This query corresponds to the Rhea website query:
+			# https://www.rhea-db.org/rhea?query=ec:*
+			#
+			SELECT ?ec ?ecNumber ?rhea
+			WHERE {
+			  ?rhea rdfs:subClassOf rh:Reaction .
+			  ?rhea rh:accession 'lala' .
+			  ?rhea rh:ec ?ec .
+			  BIND(strafter(str(?ec),str(ec:)) as ?ecNumber)
+			  ?rhea rh:isTransport ?isTransport .
+			  ?rhea rh:equation [] .
+			}''' . """;
+	
 	String simple = """
 						prefix ex: <https://sparql.swisslipids.org/.well-known/sparql-examples/>
 			prefix sh: <http://www.w3.org/ns/shacl#>
@@ -95,7 +134,15 @@ public class SparqlInRdfToMermaidTest {
 	@Test
 	public void rhea9() {
 		Model model = parse(rhea9);
-		assertTrue(SparqlInRdfToMermaid.asMermaid(model).contains("rh:ec"));
+		String res = SparqlInRdfToMermaid.asMermaid(model);
+		assertTrue(res.contains("rh:ec"));
+	}
+	
+	@Test
+	public void rhea9Anon() {
+		Model model = parse(rhea9Anon);
+		String res = SparqlInRdfToMermaid.asMermaid(model);
+		assertTrue(res.contains("rh:ec"));
 	}
 
 	private Model parse(String ttl) {
