2a1bb1faf79fb5775f535899878041332873b08e
Jerven Bolleman <jerven.bolleman@sib.swiss>
2024-08-07 16:55:59 +0200

    Also change the java code to match the ontology

--- src/main/java/swiss/sib/rdf/sparql/examples/vocabularies/SIB.java
+++ src/main/java/swiss/sib/rdf/sparql/examples/vocabularies/SIB.java
@@ -5,7 +5,7 @@ import org.eclipse.rdf4j.model.impl.SimpleValueFactory;
 import org.eclipse.rdf4j.model.vocabulary.SHACL;
 
 public class SIB {
-	public static final IRI DESCRIBE = SimpleValueFactory.getInstance().createIRI(SHACL.NAMESPACE, "describe");
+	public static final IRI DESCRIBE = SimpleValueFactory.getInstance().createIRI("https://purl.expasy.org/sparql-examples#", "describe");
 	
 	public static final IRI FILE_NAME = SimpleValueFactory.getInstance().createIRI("http://example.org/", "file_name");
 	public static final IRI PROJECT = SimpleValueFactory.getInstance().createIRI("http://example.org/", "project");
