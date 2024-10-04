c5a05494366425095f01210716ec6ee5e293ac69
Jerven Bolleman <jerven.bolleman@sib.swiss>
2024-10-04 12:47:08 +0200

    Tester: output test results in markdown to help track progres

--- src/main/java/swiss/sib/rdf/sparql/examples/Tester.java
+++ src/main/java/swiss/sib/rdf/sparql/examples/Tester.java
@@ -1,15 +1,27 @@
 package swiss.sib.rdf.sparql.examples;
 
+import java.io.BufferedWriter;
+import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
+import java.nio.file.OpenOption;
 import java.nio.file.Path;
+import java.nio.file.StandardOpenOption;
+import java.text.NumberFormat;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
+import java.util.Optional;
 import java.util.concurrent.Callable;
 import java.util.regex.Pattern;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 import org.junit.platform.console.ConsoleLauncher;
+import org.junit.platform.console.ConsoleLauncherExecutionResult;
+import org.junit.platform.engine.UniqueId;
+import org.junit.platform.launcher.TestIdentifier;
+import org.junit.platform.launcher.listeners.TestExecutionSummary;
 
 import picocli.CommandLine;
 import picocli.CommandLine.Command;
@@ -20,9 +32,12 @@ import swiss.sib.rdf.sparql.examples.tests.ValidateSparqlExamplesTest;
 
 @Command(name = "test", description = "Tests the example files")
 public class Tester implements Callable<Integer> {
-	@Spec 
+	// Next cell in a markdown table
+	private static final String NC = " | ";
+
+	@Spec
 	CommandSpec spec;
-	
+
 	@Option(names = { "-i",
 			"--input-directory" }, paramLabel = "directory containing example files to test", description = "The root directory where the examples and their prefixes can be found.", required = true)
 	private Path inputDirectory;
@@ -35,6 +50,9 @@ public class Tester implements Callable<Integer> {
 
 	@Option(names = { "--also-run-slow-tests" })
 	private boolean alsoRunSlowTests;
+
+	@Option(names = { "--status-markdown" })
+	private File statusMarkdown;
 
 	@Override
 	public Integer call() throws Exception {
@@ -80,8 +98,119 @@ public class Tester implements Callable<Integer> {
 			standardOptions.add("--exclude-tag");
 			standardOptions.add("SlowTest");
 		}
-		return ConsoleLauncher.execute(System.out, System.err, (String[]) standardOptions.toArray(new String[0])).getExitCode();
-		
+		ConsoleLauncherExecutionResult execute = ConsoleLauncher.execute(System.out, System.err,
+				(String[]) standardOptions.toArray(new String[0]));
+		if (statusMarkdown != null && execute.getTestExecutionSummary().isPresent()) {
+			try (BufferedWriter w = Files.newBufferedWriter(statusMarkdown.toPath(),
+					StandardOpenOption.TRUNCATE_EXISTING, StandardOpenOption.CREATE)) {
+				TestExecutionSummary tes = execute.getTestExecutionSummary().get();
+				Map<String, List<TestExecutionSummary.Failure>> collect = tes.getFailures().stream()
+						.collect(Collectors.groupingBy((f) -> {
+							TestIdentifier ti = f.getTestIdentifier();
+							Optional<UniqueId> opid = ti.getParentIdObject();
+							if (opid.isPresent()) {
+								return opid.get().getLastSegment().getValue();
+							}
+							return ti.getUniqueId();
+						}));
+				printMarkdownSummary(w, tes);
+				w.append("# Summary regarding test failures");
+				w.newLine();
+				if (collect.containsKey("testAllWithBigData()")) {
+					List<TestExecutionSummary.Failure> bigdata = collect.get("testAllWithBigData()");
+					w.append("| test group | failed | compared to blazegraph |");
+					w.newLine();
+					w.append("| ---- | ---- | ---- |");
+					w.newLine();
+					for (Map.Entry<String, List<TestExecutionSummary.Failure>> gf : collect.entrySet()) {
+						if (!gf.getKey().equals("testAllWithBigData()") && gf.getKey().startsWith("testAllWith")) {
+							w.append(NC);
+							w.append(gf.getKey());
+							w.append(NC);
+							w.append(Integer.toString(gf.getValue().size()));
+							w.append(NC);
+							w.append(percentage(gf.getValue().size(), bigdata.size()));
+							w.append(NC);
+							w.newLine();
+						}
+					}
+				} else {
+					for (Map.Entry<String, List<TestExecutionSummary.Failure>> gf : collect.entrySet()) {
+						w.append("# Summary " + gf.getKey() + " (" + gf.getValue().size() + ")");
+						w.newLine();
+						w.append("");
+						w.newLine();
+					}
+				}
+				w.newLine();
+				w.newLine();
+				for (Map.Entry<String, List<TestExecutionSummary.Failure>> gf : collect.entrySet()) {
+					w.append("# Failures " + gf.getKey() + " (" + gf.getValue().size() + ")");
+					w.newLine();
+					w.append("| test name | exception |");
+					w.newLine();
+					w.append("| ---- | ---- |");
+					w.newLine();
+					for (var failure : gf.getValue()) {
+						String dn = failure.getTestIdentifier().getDisplayName();
+						w.append("|[").append(dn).append("](examples/").append(dn).append(")|");
+						String em = failure.getException().getMessage();
+						if (em == null) {
+							w.append(" ");
+						} else {
+							em = em.replace('\n', ' ').replace('\r', ' ');
+							if (em.length() > 80) {
+								w.append(em, 0, 80).append("...");
+							} else {
+								w.append(em);
+							}
+						}
+						w.append("|");
+						w.newLine();
+					}
+
+				}
+			}
+		}
+		return execute.getExitCode();
+
 	}
 
+	public void printMarkdownSummary(BufferedWriter w, TestExecutionSummary tes) throws IOException {
+		w.append("# Summary regarding test failures");
+		w.newLine();
+		w.newLine();
+		w.append("| total | tests | percentage");
+		w.newLine();
+		w.append("| ---- | ---- | ---- |");
+		w.newLine();
+		long found = tes.getTestsFoundCount();
+		long failed = tes.getTestsFailedCount();
+		long aborted = tes.getTestsAbortedCount();
+		long skipped = tes.getTestsSkippedCount();
+		long passed = tes.getTestsSucceededCount();
+		w.append("| found | ").append(Long.toString(found)).append(NC).append("100%|");
+		w.newLine();
+		w.append("| failed | ").append(Long.toString(failed)).append(NC).append(percentage(found - failed, found))
+				.append(NC);
+		w.newLine();
+		w.append("| aborted | ").append(Long.toString(aborted)).append(NC).append(percentage(found - aborted, found))
+				.append(NC);
+		w.newLine();
+		w.append("| skipped | ").append(Long.toString(skipped)).append(NC).append(percentage(found - skipped, found))
+				.append(NC);
+		w.newLine();
+		w.append("| passed | ").append(Long.toString(passed)).append(NC).append(percentage(found - passed, found))
+				.append(NC);
+		w.newLine();
+		w.newLine();
+	}
+
+	public String percentage(long part, long whole) {
+		if (part > 0) {
+			return NumberFormat.getPercentInstance().format((part / (double) whole));
+		} else {
+			return "-";
+		}
+	}
 }
