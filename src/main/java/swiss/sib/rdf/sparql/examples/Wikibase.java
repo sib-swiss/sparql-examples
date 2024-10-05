a7ad0642d059abb0f3789e9417bee7dfaee2edff
Jerven Bolleman <jerven.bolleman@sib.swiss>
2024-10-05 17:20:26 +0200

    Wikibase: improve comment extraction

--- src/main/java/swiss/sib/rdf/sparql/examples/Wikibase.java
+++ src/main/java/swiss/sib/rdf/sparql/examples/Wikibase.java
@@ -1,24 +1,37 @@
 package swiss.sib.rdf.sparql.examples;
 
+import java.io.ByteArrayInputStream;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.OutputStream;
 import java.net.SocketTimeoutException;
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.net.URLEncoder;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpRequest.Builder;
+import java.net.http.HttpResponse;
+import java.net.http.HttpResponse.BodyHandlers;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.StandardOpenOption;
 import java.nio.file.attribute.FileTime;
 import java.time.Duration;
 import java.time.Instant;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
 import java.time.format.DateTimeFormatter;
 import java.time.temporal.TemporalAccessor;
 import java.util.List;
 import java.util.concurrent.Callable;
 import java.util.regex.Pattern;
 import java.util.stream.Collectors;
+import java.util.zip.GZIPInputStream;
 
 import org.eclipse.rdf4j.model.IRI;
 import org.eclipse.rdf4j.model.Model;
@@ -32,8 +45,6 @@ import org.eclipse.rdf4j.model.vocabulary.SHACL;
 import org.eclipse.rdf4j.query.algebra.evaluation.function.hash.MD5;
 import org.eclipse.rdf4j.rio.RDFFormat;
 import org.eclipse.rdf4j.rio.Rio;
-import org.jsoup.Connection;
-import org.jsoup.Connection.Method;
 import org.jsoup.Connection.Response;
 import org.jsoup.HttpStatusException;
 import org.jsoup.Jsoup;
@@ -44,6 +55,9 @@ import org.jsoup.nodes.TextNode;
 import org.jsoup.select.Elements;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.sweble.wikitext.parser.WikitextParser;
+import org.sweble.wikitext.parser.nodes.WtNode;
+import org.sweble.wikitext.parser.utils.SimpleParserConfig;
 
 import picocli.CommandLine;
 import picocli.CommandLine.Command;
@@ -52,6 +66,7 @@ import picocli.CommandLine.Option;
 import picocli.CommandLine.Spec;
 import swiss.sib.rdf.sparql.examples.vocabularies.SIB;
 import swiss.sib.rdf.sparql.examples.vocabularies.SchemaDotOrg;
+import xtc.parser.ParseException;
 
 @Command(name = "wikibase", description = "Converts queries from wikibase wiki into sparql-example-files")
 public class Wikibase implements Callable<Integer> {
@@ -83,6 +98,8 @@ public class Wikibase implements Callable<Integer> {
 
 	private File outputSearchResultDir;
 	private File outputHtmlDir;
+	private File outputJsonDir;
+
 	private static final ValueFactory VF = SimpleValueFactory.getInstance();
 
 	private static final IRI WIKIDATA_PREFIXES = VF.createIRI("https://example.org/to_decide/wikidata_prefixes");
@@ -108,19 +125,23 @@ public class Wikibase implements Callable<Integer> {
 			if (!outputHtmlDir.exists() && !outputHtmlDir.mkdirs()) {
 				Failure.CANT_READ_INPUT_DIRECTORY.exit(null);
 			}
+			outputJsonDir = new File(outputDirectory.toFile(), "json");
+			if (!outputJsonDir.exists() && !outputJsonDir.mkdirs()) {
+				Failure.CANT_READ_INPUT_DIRECTORY.exit(null);
+			}
 			fetchHtml();
 		}
 		return 0;
 	}
 
-	private enum NamedTemplate {
+	enum NamedTemplate {
 		SPARQL("SPARQL", ".mw-highlight-lang-sparql"),
 //		WDQUERY("Wdquery", ".mw-highlight-lang-sparql"),
 //		SPARQL_INLINE("SPARQL_Inline/doc", ".mw-highlight-lang-sparql"),
 		SPARQL2("SPARQL2", ".mw-highlight-lang-sparql"),;
 
 		private final String name;
-		private final String cssClass;
+		final String cssClass;
 
 		NamedTemplate(String name, String cssClass) {
 			this.name = name;
@@ -132,19 +153,23 @@ public class Wikibase implements Callable<Integer> {
 	private void fetchHtml() {
 
 		for (NamedTemplate nt : NamedTemplate.values()) {
-			String url = wikidatawiki + "w/index.php?title=Special:WhatLinksHere/Template:" + nt.name;
+			String url = wikidatawiki + "/w/index.php?title=Special:WhatLinksHere/Template:" + nt.name;
 
 			try {
 				while (url != null && !url.equals(wikidatawiki)) {
 					Document searchResultDocument = retrieveAndCacheResult(url, outputSearchResultDir);
-					Elements ulWhatLinksHereList = searchResultDocument.select("#mw-whatlinkshere-list li > a");
-					for (Element link : ulWhatLinksHereList) {
-						String pageLinkingToSparqlTemplate = makeAbsoluteUrl(link);
-						log.info(pageLinkingToSparqlTemplate);
-						extractSparqlQueriesFromPage(pageLinkingToSparqlTemplate, nt);
+					if (searchResultDocument == null) {
+						url = null;
+					} else {
+						Elements ulWhatLinksHereList = searchResultDocument.select("#mw-whatlinkshere-list li > a");
+						for (Element link : ulWhatLinksHereList) {
+							String pageLinkingToSparqlTemplate = makeAbsoluteUrl(link);
+							log.info(pageLinkingToSparqlTemplate);
+							extractSparqlQueriesFromPage(pageLinkingToSparqlTemplate, nt);
+						}
+						Element nextLink = searchResultDocument.select(".mw-nextlink").first();
+						url = makeAbsoluteUrl(nextLink);
 					}
-					Element nextLink = searchResultDocument.select(".mw-nextlink").first();
-					url = makeAbsoluteUrl(nextLink);
 				}
 			} catch (IOException e) {
 				Failure.CONNECTION_TO_WIKIDATA_WIKI_FAIL.exit(e);
@@ -153,6 +178,8 @@ public class Wikibase implements Callable<Integer> {
 	}
 
 	private String makeAbsoluteUrl(Element link) {
+		if (link == null)
+			return null;
 		String pageLinkingToSparqlTemplate = link.attr("href");
 		if (pageLinkingToSparqlTemplate.startsWith("/")) {
 			pageLinkingToSparqlTemplate = wikidatawiki + pageLinkingToSparqlTemplate;
@@ -171,20 +198,23 @@ public class Wikibase implements Callable<Integer> {
 		if (htmlPageDocument == null) {
 			System.err.println("Could not retrieve:" + pageLinkingToSparqlTemplate);
 		} else {
-			String languageInHtml = htmlPageDocument.child(0).attr("lang");
 
+			String languageInHtml = detectLanguageOfPage(htmlPageDocument);
 			Elements sparqlTemplates = htmlPageDocument.select(nt.cssClass);
 			for (Element sparqlTemplate : sparqlTemplates) {
-				//Concat the SPARQL string undo " escaping.
-				String query = sparqlTemplate.select("pre").eachText().stream().collect(Collectors.joining("\n"))
-						.replace("\\\"", "\"").replace("<nowiki>", "").replace("</nowiki>", "");
+				// Concat the SPARQL string undo " escaping.
+				List<String> queryParts = sparqlTemplate.select("pre").eachText().stream().toList();
+
+				String query = queryParts.stream().collect(Collectors.joining("\n")).replace("\\\"", "\"")
+						.replace("<nowiki>", "").replace("</nowiki>", "");
 				if (query != null && !query.isBlank()) {
 					Model model = new TreeModel();
 					String urlForFileName = new MD5().evaluate(VF, VF.createLiteral(query)).stringValue();
 					IRI iriForQuery = VF.createIRI(wikidatawiki + "#query-" + urlForFileName);
-					//If we can't add a query string we should not try to store it, as there is nothing to show.
+					// If we can't add a query string we should not try to store it, as there is
+					// nothing to show.
 					if (addQueryStringToModel(query, model, iriForQuery)) {
-						extractComment(languageInHtml, sparqlTemplate, model, iriForQuery, nt);
+						extractComment(languageInHtml, sparqlTemplate, model, iriForQuery, nt, queryParts);
 						model.add(iriForQuery, DCTERMS.IS_PART_OF, VF.createIRI(pageLinkingToSparqlTemplate));
 						model.add(iriForQuery, DCTERMS.LICENSE, CC_BY_4);
 						model.add(iriForQuery, SHACL.PREFIXES, WIKIDATA_PREFIXES);
@@ -196,52 +226,134 @@ public class Wikibase implements Callable<Integer> {
 		}
 	}
 
-	private void extractComment(String languageInHtml, Element sparqlTemplate, Model model, IRI iriForQuery, NamedTemplate nt) {
-		StringBuilder sb = makeThePreviousSiblingNodesTheLabel(sparqlTemplate, nt);
-//		if (!sb.isEmpty() && languageInHtml != null && !languageInHtml.isBlank()) {
-//			model.add(iriForQuery, RDFS.COMMENT, VF.createLiteral(sb.toString(), languageInHtml));
-//		} else if (!sb.isEmpty()) {
-//			model.add(iriForQuery, RDFS.COMMENT, VF.createLiteral(sb.toString()));
-//		} else {
-			model.add(iriForQuery, RDFS.COMMENT, VF.createLiteral("TODO", "en"));
-//		}
+	public String detectLanguageOfPage(Document htmlPageDocument) {
+		String languageInHtml = htmlPageDocument.child(0).attr("lang");
+		//Look to see if there is a translate bucket.
+		Elements languages = htmlPageDocument.select(".mw-pt-languages-selected");
+		if (languages != null && ! languages.isEmpty()) {
+			languageInHtml = languages.first().attr("lang");
+		}
+		return languageInHtml;
 	}
+
+	private static final WikitextParser WIKITEXT_PARSER = new WikitextParser(new SimpleParserConfig());
+
+	public static WtNode parseWikiText(String title, String content) {
+		try {
+			WtNode article = WIKITEXT_PARSER.parseArticle(content, title);
+			return article;
+		} catch (IOException | ParseException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			return null;
+		}
+	}
+
+	static void extractComment(String languageInHtml, Element sparqlTemplate, Model model, IRI iriForQuery,
+			NamedTemplate nt, List<String> queryParts) {
+		String sb = makeThePreviousSiblingNodesTheLabel(sparqlTemplate, nt);
+		String commentInQuery = commentInQuery(queryParts);
+		if (!commentInQuery.isBlank()) {
+	    	model.remove(iriForQuery, RDFS.COMMENT, VF.createLiteral("TODO", languageInHtml));
+			model.add(iriForQuery, RDFS.COMMENT, VF.createLiteral(commentInQuery, languageInHtml));
+	    } else if (!sb.isEmpty()) {
+			model.remove(iriForQuery, RDFS.COMMENT, VF.createLiteral("TODO", languageInHtml));
+			model.add(iriForQuery, RDFS.COMMENT, VF.createLiteral(sb, languageInHtml)); 
+		} else if (! model.getStatements(iriForQuery, RDFS.COMMENT, null).iterator().hasNext()){ //Don't add a TODO if we already have something
+			model.add(iriForQuery, RDFS.COMMENT, VF.createLiteral("TODO", languageInHtml));
+		}
+	}
+
+	public static String commentInQuery(List<String> queryParts) {
+		return queryParts.stream().takeWhile(s -> s.startsWith("#")).map(s -> s.substring(1))
+				.filter(s -> !s.contains("defaultView:")).collect(Collectors.joining(" "));
+	}
+
 //
-	private StringBuilder makeThePreviousSiblingNodesTheLabel(Element sparqlTemplate, NamedTemplate nt) {
-		Element parent = sparqlTemplate.parent();
-		List<Node> childNodes = parent.childNodes();
+	static String makeThePreviousSiblingNodesTheLabel(Element sparqlTemplate, NamedTemplate nt) {
+		Element grandParent = sparqlTemplate.parent().parent();
 		StringBuilder sb = new StringBuilder();
+		if (grandParent.hasClass("wdt-sparql-container")) {
+			Node parent = grandParent;
+			while (parent.previousSibling() != null) {
+				
+				Node prev = parent.previousSibling();
+				List<Node> prevC = prev.childNodes();
+				if(stop(prev)) {
+					break;
+				}
+				if(! extractFromNodes(sparqlTemplate, nt, prevC, sb)) {
+					parent = prev;
+				} else {
+					break;
+				}
+			}
+			return clean(sb);
+		} else {
+			Node parent = sparqlTemplate;
+			while (parent.previousSibling() != null) {
+				if (!extractFromNodes(sparqlTemplate, nt, parent.previousSibling().childNodes(), sb)) {
+					parent = parent.previousSibling();
+				} else {
+					break;
+				}
+			}
+			return clean(sb);
+		}
+	}
+
+	static boolean stop(Node prev){
+		if (prev instanceof Element e) {
+		return e.hasClass("wdt-sparql-container") || e.hasClass("mw-highlight-lang-sparql");
+		} else {
+			return false;
+		}
+	}
+
+	public static boolean extractFromNodes(Element sparqlTemplate, NamedTemplate nt, List<Node> childNodes, StringBuilder sb) {
+
 		for (Node childNode : childNodes) {
 			if (childNode.equals(sparqlTemplate)) {
-				return sb;
+				return true;
 			} else {
 				if (childNode instanceof TextNode tn) {
-					sb.append(tn.text());
-				} else if (childNode instanceof Element e) {
-					if (!e.getElementsByClass(nt.cssClass).isEmpty()) {
-						return clean(sb);
-					} else if (e.nameIs("h1") || e.nameIs("h2") || e.nameIs("h3") || e.nameIs("h4") || e.nameIs("h5") || e.nameIs("h6")) {
-						sb.append(e.text());
-						return sb;
+					if (! tn.text().equals("Try it!")) {
+						sb.insert(0, tn.text());
 					}
-					sb.append(e.text());
+				} else if (childNode instanceof Element e) {
+					String text = e.text();
+					if (text.contains("Items used") || text.contains("Properties used") || text.contains("Features used")) {
+						//Skip form SPARQL2
+					} else if (!e.getElementsByClass(nt.cssClass).isEmpty()) {
+						return true;
+					} else if(e.hasClass("wdt-sparql-container") || e.hasClass("mw-highlight-lang-sparql")) {
+						return true;
+					} else if (e.nameIs("h1") || e.nameIs("h2") || e.nameIs("h3") || e.nameIs("h4") || e.nameIs("h5")
+							|| e.nameIs("h6") || e.hasClass("mw-heading")) {
+						sb.insert(0, e.text());
+						return true;
+					} else {
+						if(extractFromNodes(sparqlTemplate, nt, e.childNodes(), sb))
+							return true;
+					}
+//					if (fromNodes)
+//						return fromNodes;					
 				}
 			}
 		}
-		return clean(sb);
+		return false;
 	}
 
-	//From the template sparql2 we don't want this part of the text.
-	private StringBuilder clean(StringBuilder sb) {
+	// From the template sparql2 we don't want this part of the text.
+	private static String clean(StringBuilder sb) {
 		int indexOf = sb.indexOf("The following query uses these:");
 		if (indexOf > 0) {
 			sb.setLength(indexOf);
 		}
-		return sb;
+		return sb.toString().strip();
 	}
 
-	private void writeModelToTurtle(Model model, String urlForFileName)
-			throws IOException, FileNotFoundException {
+	private void writeModelToTurtle(Model model, String urlForFileName) throws IOException, FileNotFoundException {
 		File turtleDestination = new File(outputDirectory.toFile(),
 				urlForFileName + "." + RDFFormat.TURTLE.getDefaultFileExtension());
 		// If the turle file already exists we have seen this query before.
@@ -282,6 +394,8 @@ public class Wikibase implements Callable<Integer> {
 		return true;
 	}
 
+	private static final HttpClient CLIENT = HttpClient.newHttpClient();
+
 	private Document retrieveAndCacheResult(String url, File output) throws IOException {
 
 		String urlForFileName = URLEncoder.encode(url, StandardCharsets.UTF_8);
@@ -297,13 +411,10 @@ public class Wikibase implements Callable<Integer> {
 			int tries = 0;
 			do {
 				try {
-					tries++;
-					Connection connect = Jsoup.connect(url);
-					connect.userAgent(yourEmail).method(Method.GET);
-					Response response = connect.execute();
-					if (response.statusCode() == 200) {
-						return parseAndCacheSuccefullResponse(cacheLocation, response);
+					if (download(url, cacheLocation, yourEmail)) {
+						return Jsoup.parse(cacheLocationF);
 					}
+					tries++;
 					sleep(100);
 				} catch (SocketTimeoutException | HttpStatusException ste) {
 					// If we had a socket timeout we sleep longer before trying again.
@@ -314,6 +425,72 @@ public class Wikibase implements Callable<Integer> {
 		}
 	}
 
+	private String retrieveAndCacheJson(String url, File output) throws IOException {
+		String urlForFileName = URLEncoder.encode(url, StandardCharsets.UTF_8);
+		if (urlForFileName.length() > 255) {
+			urlForFileName = urlForFileName.substring(0, 255);
+		}
+		File cacheLocationF = new File(output, urlForFileName);
+		Path cacheLocation = cacheLocationF.toPath();
+		if (Files.exists(cacheLocation)
+				&& (useCached || Files.getLastModifiedTime(cacheLocation).toInstant().isAfter(Instant.now()))) {
+			return Files.readString(cacheLocation);
+		} else {
+			int tries = 0;
+			do {
+				try {
+					if (download(url, cacheLocation, yourEmail)) {
+						return Files.readString(cacheLocation);
+					}
+					tries++;
+					log.info("Try:" + tries + " Could not download " + url);
+					sleep(100);
+				} catch (SocketTimeoutException | HttpStatusException ste) {
+					// If we had a socket timeout we sleep longer before trying again.
+					sleep(1000);
+				}
+			} while (tries <= 3);
+			return null;
+		}
+	}
+
+	public static boolean download(String url, Path out, String yourEmail) {
+		try {
+			Builder cb = HttpRequest.newBuilder(new URI(url));
+			cb.setHeader("User-Agent", yourEmail);
+			cb.setHeader("Accept-Encoding", "gzip,");
+			if (Files.exists(out)) {
+				Instant instant = Files.getLastModifiedTime(out).toInstant();
+				ZonedDateTime ldt = ZonedDateTime.ofInstant(instant, ZoneId.of("GMT"));
+				cb.setHeader("If-Modified-Since", DateTimeFormatter.RFC_1123_DATE_TIME.format(ldt));
+			}
+			cb.GET();
+			HttpResponse<byte[]> send = CLIENT.send(cb.build(), BodyHandlers.ofByteArray());
+			if (send.statusCode() == 200) {
+				saveResponse(out, send);
+				return true;
+			} else if (send.statusCode() == 304) {
+				return true;
+			}
+			return false;
+		} catch (URISyntaxException | IOException | InterruptedException e) {
+			log.info("Issue with ", e);
+		}
+		return false;
+	}
+
+	public static void saveResponse(Path out, HttpResponse<byte[]> send) throws IOException {
+		if (send.headers().firstValue("content-encoding").isPresent()) {
+			try (GZIPInputStream gzipInputStream = new GZIPInputStream(new ByteArrayInputStream(send.body()));
+					OutputStream os = Files.newOutputStream(out, StandardOpenOption.TRUNCATE_EXISTING,
+							StandardOpenOption.CREATE)) {
+				gzipInputStream.transferTo(os);
+			}
+		} else {
+			Files.write(out, send.body(), StandardOpenOption.TRUNCATE_EXISTING, StandardOpenOption.CREATE);
+		}
+	}
+
 	private Document parseAndCacheSuccefullResponse(Path cacheLocation, Response response) throws IOException {
 		Document document;
 		byte[] bodyAsBytes = response.bodyAsBytes();
@@ -321,8 +498,12 @@ public class Wikibase implements Callable<Integer> {
 		Files.write(cacheLocation, bodyAsBytes);
 
 		// Wikipedia asks us to sleep for 100ms between requests
-
-		if (response.hasHeader("expires")) {
+		if (response.hasHeader("last-modified")) {
+			String expires = response.header("last-modified");
+			TemporalAccessor temporal = DateTimeFormatter.RFC_1123_DATE_TIME.parse(expires);
+			Instant expiresInstant = Instant.from(temporal);
+			Files.setLastModifiedTime(cacheLocation, FileTime.from(expiresInstant));
+		} else if (response.hasHeader("expires")) {
 			String expires = response.header("expires");
 			TemporalAccessor temporal = DateTimeFormatter.RFC_1123_DATE_TIME.parse(expires);
 			Instant expiresInstant = Instant.from(temporal);
